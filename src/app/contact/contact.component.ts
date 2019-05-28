ce48f173a105ad5de83b737b035195eeea8d16b7
kcaayush2158 <aayushkc2158@gmail.com>
2019-05-28 22:01:08 +0545

    Update contact.component.ts

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,17 +3,23 @@ import { Component, OnInit, Input,Output,EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  template:'<div *ngIf="displayName; then ifBlock else elseBlock" > </div> <br> <ng-template #ifBlock> <h2> IF statement is running</h2></ng-template>  <ng-template #elseBlock> <h2> Else statement is running</h2></ng-template>',
+  template:'<br><input [(ngModel)]="name" type="text"> {{name}}',
   styles: []
 })
 
 
 export class ContactComponent implements OnInit {
 
-  public displayName = false;
-  ngOnInit(): void {
-    throw new Error("Method not implemented.");
-  }
+public name="";
 
+    constructor(){
 
+    }
+    ngOnInit(){
+
+    }
+    logMessage(value){
+      console.log(value)
+
+    }
 }
